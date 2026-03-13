51e14c8c0d21dbed39a4cb9e0a435565093d5797
Christian Langen <christian.langen@gmail.com>
2026-03-13 19:40:14 +0100

    refactor dir structure

--- retroimage/tests/test_compress.rs
+++ retroimage/tests/test_compress.rs
@@ -7,13 +7,13 @@ use std::path::Path;
 
 #[test]
 fn test_image_compress() -> Result<()> {
-    let path = Path::new("../images/IMG_2638.JPG");
+    let path = Path::new("../../images/IMG_2638.JPG");
     let img = ImageReader::open(path)?.decode()?;
     let th = img.thumbnail(576, 720); // .. stores a fixed 576x720 pixel image using
     let mut gray: GrayImage = th.grayscale().into();
     diffuse_image(&mut gray, Diffusion::FloydSteinberg, 128u8);
 
     let output = compress_image(&gray)?;
-    fs::write("../images/IMG_2638.mcpaint", output).expect("Fehler beim Schreiben");
+    fs::write("../../images/IMG_2638.mcpaint", output).expect("Fehler beim Schreiben");
     Ok(())
 }
