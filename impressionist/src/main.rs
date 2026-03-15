f8066bf376801059e73849c7acd1fea6c03172eb
Christian Langen <christian.langen@gmail.com>
2026-03-15 07:36:34 +0100

    random shapes

--- impressionist/src/main.rs
+++ impressionist/src/main.rs
@@ -1,9 +1,76 @@
 use image::{Pixel, Rgb, RgbImage};
-use imageproc::drawing::draw_filled_circle_mut;
+use imageproc::drawing::{draw_filled_ellipse_mut, draw_line_segment_mut, draw_polygon_mut};
+use imageproc::point::Point;
+use itertools::izip;
+use rand::RngExt;
 use show_image::event;
 use show_image::{ImageInfo, ImageView, create_window};
 
-use itertools::izip;
+#[allow(unused)]
+enum ShapeType {
+    Ellipse,
+    Triangle,
+    Quadrinial,
+    Line,
+}
+
+struct Shape {
+    shape_type: ShapeType,
+    points: Vec<Point<i32>>,
+}
+
+fn random_point(screen_width: u32, screen_height: u32) -> Point<i32> {
+    let mut rng = rand::rng();
+    let x = rng.random_range(0..screen_width) as i32;
+    let y = rng.random_range(0..screen_height) as i32;
+    Point { x, y }
+}
+
+fn random_color() -> Rgb<u8> {
+    let mut rng = rand::rng();
+    let colors: [u8; 3] = std::array::from_fn(|_| rng.random_range(0..=255));
+    Rgb(colors)
+}
+
+impl Shape {
+    fn new(shape_type: ShapeType, screen_width: u32, screen_height: u32) -> Self {
+        let num_points = match shape_type {
+            ShapeType::Line => 2,
+            ShapeType::Ellipse => 2,
+            ShapeType::Triangle => 3,
+            ShapeType::Quadrinial => 4,
+        };
+
+        let points = (0..num_points)
+            .map(|_| random_point(screen_width, screen_height))
+            .collect();
+        Self { shape_type, points }
+    }
+
+    fn draw(&self, canvas: &mut RgbImage) {
+        let color = random_color();
+
+        match self.shape_type {
+            ShapeType::Ellipse => {
+                // convert bounding-box points to -> center & radius
+                let delta = self.points[1] - self.points[0];
+                let dx = delta.x / 2;
+                let dy = delta.y / 2;
+                let center = (self.points[0].x + dx, self.points[0].y + dy);
+                draw_filled_ellipse_mut(canvas, center, dx.abs(), dy.abs(), color);
+            }
+
+            ShapeType::Line => {
+                let start = (self.points[0].x as f32, self.points[0].y as f32);
+                let end = (self.points[1].x as f32, self.points[1].y as f32);
+                draw_line_segment_mut(canvas, start, end, color);
+            }
+            ShapeType::Triangle | ShapeType::Quadrinial => {
+                draw_polygon_mut(canvas, &self.points, color);
+            }
+        }
+    }
+}
 
 #[allow(unused)]
 fn subtract_images(img1: &RgbImage, img2: &RgbImage) -> RgbImage {
@@ -27,13 +94,9 @@ fn calculate_difference(img1: &RgbImage, img2: &RgbImage) -> u64 {
         })
 }
 
-#[show_image::main]
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // 1. Erstelle ein Bild mit dem image-Crate (z.B. 800x600, schwarz)
-    let width = 800;
-    let height = 600;
-    let mut img = RgbImage::new(width, height);
-
+#[allow(unused)]
+fn snippets() {
+    /*
     // 2. Ein paar Pixel manuell setzen (oder hier imageproc nutzen)
     for x in 0..width {
         for y in 0..height {
@@ -41,16 +104,24 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             img.put_pixel(x, y, color);
         }
     }
+    */
+    todo!()
+}
 
-    let color = Rgb([100, 200, 240]);
+#[show_image::main]
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // 1. Erstelle ein Bild mit dem image-Crate (z.B. 800x600, schwarz)
+    let width = 800;
+    let height = 600;
+    let mut img = RgbImage::new(width, height);
 
-    draw_filled_circle_mut(&mut img, (200, 200), 100, color);
-
-    // 3. Das Bild für show-image vorbereiten
-    // Wir sagen show-image, wie die Rohdaten (Bytes) zu interpretieren sind
+    // --- print to image
+    for _ in 0..100 {
+        let shape = Shape::new(ShapeType::Ellipse, width, height);
+        shape.draw(&mut img);
+    }
+    //draw_filled_circle_mut(&mut img, (100, 100), 200, Rgb([100, 200, 200]));
     let image_view = ImageView::new(ImageInfo::rgb8(width, height), &img);
-
-    // Create a window with default options and display the image.
     let window = create_window("image", Default::default())?;
     window.set_image("bild-001", image_view)?;
 
