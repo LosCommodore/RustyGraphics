44030de88006459e276e7275f0784133a7f1cbcc
Christian Langen <christian.langen@gmail.com>
2026-03-17 22:11:59 +0100

    dev

--- impressionist/src/optimizers.rs
+++ impressionist/src/optimizers.rs
@@ -1,2 +1,8 @@
+mod no_optimizer;
 mod simple_optimizer;
+
+#[allow(unused)]
+pub use no_optimizer::no_optimizer;
+
+#[allow(unused)]
 pub use simple_optimizer::cross_optimizer;
---
+++ impressionist/src/optimizers/no_optimizer.rs
@@ -0,0 +1,18 @@
+use crate::{
+    painting::{FitnessFn, OptimizerFn},
+    shape::Shape,
+};
+
+#[allow(unused)]
+pub fn no_optimizer<'a>(
+    width: u32,
+    height: u32,
+    initial_shape: &Shape,
+    initial_score: u64,
+    fitness_function: &FitnessFn<'a>,
+) -> Option<(Shape, u64)> {
+    Some((initial_shape.clone(), initial_score))
+}
+
+// Dieser "Check" stellt sicher, dass die Signatur exakt passt:
+const _: OptimizerFn = no_optimizer;
